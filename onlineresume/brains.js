e280503a1b5edcec633bfb0433ec85ed79e88d03
unknown <zoehldan000@warren.k12.in.us>
2018-12-19 12:14:54 -0500

    final

--- onlineresume/brains.js
+++ onlineresume/brains.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    
+    $("p, li").hover(function(){
+        $(this).animate({
+            fontSize:"1.25em"
+        },1000)
+    })
+    $("p, li").mouseleave(function(){
+        $(this).animate({
+            fontSize:"1em"
+        },1000)
+    })
 })
