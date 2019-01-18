25b4fd4acf364e75dbdd576509c704699dce59c4
unknown <zoehldan000@warren.k12.in.us>
2019-01-18 13:16:07 -0500

    final

--- madlib/brains.js
+++ madlib/brains.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("#madbtn").click(function(){
-        $("#lib").css({"fontSize":"0.1em","opacity":"0"});
+        $("#lib").css({"fontSize":"0.1em","opacity":"0","backgroundSize":"100% 130%"});
+        $("footer").css({"position":"relative"});
         color=$("#color").val();
         superlative=$("#superlative").val();
         ad1=$("#ad1").val();
@@ -14,13 +15,13 @@ $(document).ready(function(){
         $("#lib").text("The "+color+" Dragon is the "+superlative+" Dragon of all. It has "+ad1+" "+bp1+", and a "+bp2+" shaped like a "+noun+". It loves to eat "+animal+", although it will feast on nearly anything. It is "+ad2+" and "+ad3+". You must be "+ad4+" around it, or you may end up as it`s meal!").show(5000).animate({"fontSize":"1.5em","opacity":"1"})
     });
 });
-var color="";
-var superlative="";
-var ad1="";
-var bp1="";
-var bp2="";
-var noun="";
-var animal="";
-var ad2="";
-var ad3="";
-var ad4="";
+var color;
+var superlative;
+var ad1;
+var bp1;
+var bp2;
+var noun;
+var animal;
+var ad2;
+var ad3;
+var ad4;
