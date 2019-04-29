5613cb281b68dd43d77c66ec25d09e43f1e33cc3
unknown <zoehldan000@warren.k12.in.us>
2019-04-29 13:00:57 -0400

    ...

--- Checkers/brains.js
+++ Checkers/brains.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
-    if($("#p1turn").css("opacity")==0.5){
+    if($("#p1turn").css("opacity")=="0.5"){
 
+        $("#p1turn").css("opacity:1");
+        $("#p2turn").css("opacity:0.5");
     }
     else if($("#p2turn").css("opacity")==0.5){
         
+        $("#p2turn").css("opacity:1");
+        $("#p1turn").css("opacity:0.5");
     }
+    $("button").click(function(){
+        $("").addClass("red")
+        $("").addClass("black")
+    })
 })
